6afc4f8b39cc0e89ef4c611d0b28da78ca901cfe
Yi Lin (Kyle) Gao <67448832+kyle-gao@users.noreply.github.com>
2021-06-23 11:57:40 -0400

    Update tf_sampling_compile.sh

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -6,4 +6,4 @@ CUDA_LIB='/usr/local/cuda-11.0/lib64'
 TF_CFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
 TF_LFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
 
-g++ -std=c++11 -shared tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -I$CUDA_INC -lcudart -L$CUDA_LIB -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 -shared tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -I$CUDA_INC -lcudart -L$CUDA_LIB -O2
